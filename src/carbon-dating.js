68e5cf6521790b09c7580377c271fc04c0dc452b
Illia Sakharau <sakharovia0204@gmail.com>
2023-04-13 19:13:19 +0400

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity = "false") {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (typeof sampleActivity === 'string' 
+      && sampleActivity.search(/[^0-9.]/) === -1 
+      && !!(+sampleActivity) 
+      && +sampleActivity < MODERN_ACTIVITY) {
+    return Math.ceil((HALF_LIFE_PERIOD / 0.693) * Math.log(MODERN_ACTIVITY / +sampleActivity))
+  } else {
+    return false
+  }
+  
+
 }
 
 module.exports = {
