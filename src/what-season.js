16e1fa92a0b8bbfffa1858bdd748474b45766bbd
Illia Sakharau <sakharovia0204@gmail.com>
2023-04-15 19:40:35 +0400

    feat: transform array

--- src/what-season.js
+++ src/what-season.js
@@ -15,41 +15,45 @@ function getSeason(date) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 
+
   if (!date){
     return "Unable to determine the time of year!";
   }
-  console.log(date);
-  console.log(date instanceof Date);
 
-
+  if (date instanceof Date) {
+    
     switch (date.getMonth()) {
-      case 11:
-        return "winter";
-      case 0:
-        return "winter";
-      case 1:
-        return "winter";
-      case 2:
-        return "spring";
-      case 3:
-        return "spring";
-      case 4:
-        return "spring";
-      case 5:
-        return "summer";
-      case 6:
-        return "summer";
-      case 7:
-        return "summer";
-      case 8:
-        return "autumn";
-      case 9:
-        return "autumn";
-      case 10:
-        return "autumn";    
-      default:
-        return 'Invalid date!';
+        case 11:
+          return "winter";
+        case 0:
+          return "winter";
+        case 1:
+          return "winter";
+        case 2:
+          return "spring";
+        case 3:
+          return "spring";
+        case 4:
+          return "spring";
+        case 5:
+          return "summer";
+        case 6:
+          return "summer";
+        case 7:
+          return "summer";
+        case 8:
+          return "autumn";
+        case 9:
+          return "autumn";
+        case 10:
+          return "autumn";    
+        default:
+          throw new Error("Invalid date!");
     }
+  } else {
+    throw new Error("Invalid date!");
+  }
+
 
 }
 
