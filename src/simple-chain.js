8e7656083a0cd3a6bd1617651ca9f95438bcc50f
Illia Sakharau <sakharovia0204@gmail.com>
2023-04-16 02:15:13 +0400

    feat: chain maker

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,25 +5,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chainArr: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chainArr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value = '(  )') {
+    this.chainArr.push(`( ${value} )`);
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  removeLink(position) {
+
+    if (Number.isInteger(position) && position <= this.chainArr.length && position > 0) {
+      this.chainArr.splice(position - 1, 1);
+    } else {  
+      this.chainArr = [];      
+      throw new Error('You can\'t remove incorrect link!');
+    }
+    return chainMaker;
+    
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chainArr.reverse();
+    return chainMaker;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let res = this.chainArr.join('~~');
+    this.chainArr = [];    
+    return res;
   }
 };
 
