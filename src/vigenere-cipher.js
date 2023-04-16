65ce28687c6de51bb30f8225591546ccf207c245
Illia Sakharau <sakharovia0204@gmail.com>
2023-04-16 23:20:02 +0400

    feat: Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,114 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  
+  constructor(type = true) {
+    this.isDirect = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    const delta = 65;
+    let res = [];
+    let spaceIndex = [];
+
+    let index = 1;
+    while (message.indexOf(' ', index) > -1) {
+      index = message.indexOf(' ', index) + 1;
+      spaceIndex.push(index - 1);
+    }
+
+    message = message.replace(/\s+/g, '');
+    
+
+    while (key.length < message.length) {
+      key += key;
+    }
+
+    for (let i = 0; i < message.length; i++) {
+
+      if (message.codePointAt(i) >= 65 && message.codePointAt(i) <= 90) {
+
+        let codePoint = message.codePointAt(i) + key.codePointAt(i) - delta;
+        if (codePoint <= 90) {
+          res.push(String.fromCharCode(codePoint));
+        } else {
+          res.push(String.fromCharCode(codePoint - 26));
+        }        
+
+      } else {
+        res.push(message[i]);
+      }
+
+    }
+
+    spaceIndex.forEach(elem => res.splice(elem, 0, " "));
+
+    if (this.isDirect) {
+      return res.join('');
+    } else {
+      return res.reverse().join('');
+    }
+    
+  }
+
+  decrypt(encryptedMessage, key) {
+
+    if (!encryptedMessage || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    const delta = 65;
+    let res = [];
+    let spaceIndex = [];
+
+    let index = 1;
+    while (encryptedMessage.indexOf(' ', index) > -1) {
+      index = encryptedMessage.indexOf(' ', index) + 1;
+      spaceIndex.push(index - 1);
+    }
+
+    encryptedMessage = encryptedMessage.replace(/\s+/g, '');
+    
+
+    while (key.length < encryptedMessage.length) {
+      key += key;
+    }
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+
+      if (encryptedMessage.codePointAt(i) >= 65 && encryptedMessage.codePointAt(i) <= 90) {
+
+        let codePoint = encryptedMessage.codePointAt(i) - key.codePointAt(i) + delta;
+        if (codePoint >= 65) {
+          res.push(String.fromCharCode(codePoint));
+        } else {
+          res.push(String.fromCharCode(codePoint + 26));
+        }        
+
+      } else {
+        res.push(encryptedMessage[i]);
+      }
+
+    }
+
+    spaceIndex.forEach(elem => res.splice(elem, 0, " "));
+
+    if (this.isDirect) {
+      return res.join('');
+    } else {
+      return res.reverse().join('');
+    }
+
+
   }
 }
 
